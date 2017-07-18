8a7d8d080e70c6cd4e01b472682c1a0d7a099ee1
Paris <mili@andrew.cmu.edu>
2017-07-18 11:11:36 +0800

    Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,16 +1,32 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_wtf import Form
 from flask_bootstrap import Bootstrap 
-from wtforms import StringField,SubmitField 
-from wtforms.validators import Required
+from wtforms import StringField,SubmitField,SelectField
+from wtforms.validators import DataRequired,InputRequired
 
 from tools import search
 
 DB_PATH = 'db'
 
 class IRForm(Form): 
-	name = StringField('Please enter the statement you want to check: ',validators=[Required()]) 
-	submit = SubmitField('Submit') 
+	statement = StringField('Please enter the statement you want to check: ',validators=[DataRequired()])
+	scheme = SelectField('Please choose the weighting scheme you want to use: ', choices=[
+		(0, 'BB2Weight'),
+		(1, 'BM25PlusWeight'),
+		(2, 'BM25Weight'),
+		(3, 'BoolWeight'),
+		(4, 'CoordWeight'),
+		(5, 'DLHWeight'),
+		(6, 'DPHWeight'),
+		(7, 'IfB2Weight'),
+		(8, 'IneB2Weight'),
+		(9, 'InL2Weight'),
+		(10, 'LMWeight'),
+		(11, 'PL2PlusWeight'),
+		(12, 'PL2Weight'),
+		(13, 'TfIdfWeight'),
+		(14, 'TradWeight')], validators=[InputRequired()], default=2, coerce=int)
+	submit = SubmitField('Submit')
 
 app = Flask(__name__) 
 app.config['SECRET_KEY'] = 'key' 
@@ -18,17 +34,20 @@ bootstrap = Bootstrap(app)
 
 @app.route('/',methods=['GET','POST']) 
 def index(): 
-	name = None 
-	nameForm = IRForm()
-	ret = ''
+	statement = None
+	weightOption = 1
+	InformationForm = IRForm(request.form)
+	ret = []
 
-	if nameForm.validate_on_submit():
-		name = nameForm.name.data
-		nameForm.name.data = ''
-		tmp = 'Retrive results: \n'+search(DB_PATH, name)
-		ret = tmp.split("\n")
-	
-	return render_template('index.html',form=nameForm,name=ret) 
+	if request.method == 'POST':
+		if InformationForm.validate_on_submit():
+			statement = InformationForm.statement.data
+			weightOption = InformationForm.scheme.data
+			InformationForm.statement.data = ''
+			tmp = 'Retrive results: \n'+search(DB_PATH, statement, weightOption)
+			ret = tmp.split("\n")
+
+	return render_template('index.html',form=InformationForm,statement=ret) 
 
 if __name__ == '__main__': 
 	app.run(debug=True)
